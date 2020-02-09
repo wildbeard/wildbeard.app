9038e390f858b54c7884434a62a1c2ebe3ccbdd2
Preston Haddock <quotablesarcasm@gmail.com>
2020-02-09 10:53:53 -0700

    Reworked the entire homepage to be just as dumb

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,85 +1,82 @@
 <template>
-	<div class="home container">
-		<div class="i-dont-actually-use-css-like-this">
-            <h1>The Name's Preston | <sub>Wild Beard</sub></h1>
-        
-            <p>You'll notice this is really bland. Not much of a designer. I am, however, really good at recreating what I can see in code.</p>
-            
-            <p>Am I looking for a job? <strong>Nope!</strong> But I bought this domain and I really needed to do something with it other than host Minecraft servers.</p>
-        </div> <!-- !.i-dont-actually-use-css-like-this -->
-        
-        <div class="i-made-some-money">
-            <h2>Experience (2011 - Until I Die Prob.)</h2>
-            <ul>
-                <li>
-                    <strong><a href="https://codepen.io/wildbeard" target="_blank">CodePen</a></strong>: <span class="fancy">Penwrite/Front-Pager</span><br>
-                    I don't work for CodePen (wouldn't that be cool, though?) but I did have several picked pens. Also I tinker on it frequently.
-                </li>
-                <!-- <li>
-                    <strong><a href="https://stackoverflow.com/users/4434366/wild-beard" target="_blank" class="so">StackOverflow:</a></strong> <span class="fancy">Answerer/HELP.ME.</span><br>
-                    I also do not work here but I do enjoy browsing the "Please Help Me" section of the site.
-                </li> -->
-                <li>
-                    <strong><a href="https://fx5solutions.com" target="_blank" class="fx5">Fx5 Solutions</a>:</strong> <span class="fancy">Software Developer | 2019 - now();</span><br>
-                    I write on every glass surface in the office I can find. I also maintain, with the help of others of course, the health of the Bx5 and Dx5 SaaS platforms as well as the servers the live on.
-                </li>
-                <li>
-                    <strong><a href="https://integer.com" target="_blank" class="int">The Integer Group</a>:</strong> <span class="fancy">Full Stack Developer | 2017 - 2019</span><br>
-                    TL;DR: Develop, maintain, and Moonshot the majority of internal tools that powers the agency globally.<br>Time reporting? Got it. Onboarding? Yup. User management (including 1<sup>st</sup> and 3<sup>rd</sup> party management)? Got it. Slack integration? Sure why not. Custom Form, HR Documents, Password Management? Done. Real time notifications across the agency's internal sites? Nailed it.
-                </li>
-                <li>
-                    <strong><a href="https://liquid.fish" target="_blank" class="lf">liquidfish</a>:</strong> <span class="fancy">Full Stack Developer | 2015 - 2017</span><br>
-                     If it was made before 2018 I probably worked on it in some way or form. <sup>(Slight hyperbole but really every dev worked on every website at least once. I could list the entire workload I had but you'd give up reading it.)</sup> Contributor to the Blake Griffin and Kevin Durant projects. I also wrote a few blogposts.
+	
+    <div class="home container">
+
+        <h1>I Break, Build, and Tinker with Things</h1>
+
+        <h4>Things You Should Include on Your Personal Site:</h4>
+
+        <ol>
+            <li>
+                <strong>A Bio</strong><br>
+                Hey, my name's Preston. I'm currently 27 years old. I write for a living and have been doing so since ~2011.
+                I don't write books, blog posts (I've written three), or news articles. I write code. Mainly web based development.<br>
+                While not at work I continue to work in my spare time. If I find time outside of my spare time, I'll play games,
+                attempt to go outside, and maybe even eat or sleep.<br>
+                I have three cats. If that matters.
+            </li>
+            <li>
+                <strong>Projects</strong><br>
+                There's not much in the way of this. My <span class="strikethrough">house</span> apartment is full of projects.
+                <ul>
+                    <li>Refurbished coffee table -- Co-refurbished with my brother</li>
+                    <li>A hand made computer desk &amp; dining room table -- Co-developed with the better half</li>
+                    <li>A leather quiver</li>
+                    <li>Various Arduino based projects like a "Soil is too dry, water me before I die" thing</li>
+                    <li>
+                        Numerous tinkerings on <a href="//codepen.io/wildbeard" target="_blank">my CodePen</a> -- None of these are complex but fun none the less
+                        <ul>
+                            <li>I've had a handful of Pens picked and listed right there on the homepage </li>
+                            <li>I participated in the <a href="//codepen.io/collection/DKyvWY" target="_blank">Daily CSS Challenges</a> (when I had time) and most of those became Picked Pens<sup>*</sup></li>
+                            <li>
+                                I also jumped onto the <a href="//codepen.io/wildbeard/pen/JNQzEZ" target="_blank">Fidget Spinner Bandwagon</a> -- What do you know it became a Picked Pen<sup>*</sup><br>
+                                I don't want to say I started a trend but after this showed up on the front page the day I made it a lot of people made their own versions. Trend. Setter. :^)
+                             </li>
+                            <li>Oh, and there's this <a href="//codepen.io/wildbeard/pen/BRXEGd" target="_blank">Self Portrait</a> -- A Picked Pen<sup>*</sup></li>
+                            <li>Also this popular <a href="//codepen.io/wildbeard/pen/eZZPZr" target="_blank">Pen</a> -- I have no idea why it is so popular I thought it was dumb when I made it</li>
+                            <li><sup>*</sup> = Picked Pens mean nothing in reality only that one of the editors saw it liked it</li>
+                        </ul>
                     </li>
-                <li>
-                    <strong><a href="https://softball.org" target="_blank" class="asa">Amateur Softball Associate</a>:</strong> <span class="fancy">Developer | 2013 - 2015</span><br>
-                    Android App, .Net, ASP, PHP. Build an <a href="https://www.usasoftballstore.com/" target="_blank" class="asa">eCommerce platform</a> from scratch, support a <a href="https://www.registerusasoftball.com" target="_blank" class="asa">registration system</a> used by 300k+ users annually, video subscriptions and testing for coaching certification, WCWC Bracket Challenge hosted on ESPN? Done.
-                </li>
-                <li>
-                    <strong>AWOL:</strong> <span class="fancy">2011 - 2013</span><br>
-                    I did some freelance, odd jobs, drove cars, did construction for a day.
-                </li>
-                <li>
-                    <strong><a href="https://advancedacademics.com/" target="_blank" class="adv">Advanced Academnics</a>:</strong> <span class="fancy">Conversion Specialist | 2011 - 2011</span><br>
-                    Basically went back to school and remade a large bulk of the course material in 508 Compliance for hearing and visually impared students. If it matters I was 18 and a teamleader.
-                </li>
-            </ul>
-        </div> <!-- !.i-made-some-money -->
-        
-        <div class="a-container container">
-            
-            <div class="row">
-				<div class="what-i-will-do-for-love col-6">
-					<h2>What I Do</h2>
-					<ul>
-						<li>HTML, CSS (Duh) | <span class="fancy">8 Years</span></li>
-						<li>Javascript (Obv.) | <span class="fancy">7 Years</span></li>
-						<li>Vue | <span class="fancy">2.75 Years</span></li>
-						<li>PHP (It's good: Fight Me) | <span class="fancy">8 Years</span></li>
-						<li>Laravel: Making PHP "Fun" | <span class="fancy">3 Years</span></li>
-						<li>Mongo, MySql, PSql (I like relationships, okay?) | <span class="fancy">1.5, 6.5, 1 Years</span></li>
-						<li>NGINX | <span class="fancy">2.5 Year</span></li>
-						<li>Docker | <span class="fancy">2 Years</span></li>
-					</ul>
-				</div> <!-- !.what-i-will-do-for-love -->
-				        
-				<div class="what-i-wont-do-for-love col-6">
-					<h3>What I Don't Do</h3>
-					<ul>
-						<li>Design (You can tell, right?) | <span class="fancy">26 Years</span></li>
-						<li>.Net (Been There; Done That) | <span class="fancy">4 Years</span></li>
-						<li>Finish Lists | <span class="fancy">26 Years</span></li>
-					</ul>
-				</div> <!-- !.what-i-wont-do-for-love -->
+                    <li>I've got a few of the typical developer side projects -- You know the ones we start but never finish.</li>
+                </ul>
+            </li>
+            <li>
+                <strong>A Blog</strong><br>
+                See #1.
+            </li>
+            <li>
+                <strong>Testimonials</strong><br>
+                <blockquote>
+                    I still have a job
+                    <span class="author">- Myself</span>
+                </blockquote>
+            </li>
+            <li>
+                <strong>A Picture of Yourself</strong><br>
+                <blockquote>
+                    Oh, and there's this <a href="//codepen.io/wildbeard/pen/BRXEGd" target="_blank">Self Portrait</a> -- A Picked Pen<sup>*</sup>
+                    <span class="author">- Myself, Again</span>
+                </blockquote>
+            </li>
+            <li>
+                <strong>Contact Information</strong><br>
+                Email: My first &amp; last name @ gmail<br>
+                Github: <a href="//github.com/wildbeard" target="_blank">Click Here</a><br>
+                Twitter: <a href="//twitter.com/wild_beard" target="_blank">Nothing Earth shattering</a><br>
+                LinkedIn: <a href="//linkedin.com/in/prestonhaddock" target="_blank">Hey, It's Me!</a>
+            </li>
+            <li>
+                <strong>A Welcome Page</strong><br>
+                Uh, welcome?
+            </li>
+        </ol>
 
-			</div>
-            
-        </div> <!-- !.a-container -->
 	</div>
-</template>
-<script>
-	export default {}
-</script>
-<style lang="scss" scoped>
 
+</template>
+
+<style lang="scss" scoped>
+    ol > li {
+        margin-bottom: 1em;
+    }
 </style>
