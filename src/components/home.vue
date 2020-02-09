3eeb0b3385addcd3b9a68e2727e1731c87860b03
Preston Haddock <quotablesarcasm@gmail.com>
2020-02-09 11:55:05 -0700

    Upping the hype game

--- src/components/home.vue
+++ src/components/home.vue
@@ -17,51 +17,57 @@
             </li>
             <li>
                 <strong>Projects</strong><br>
-                There's not much in the way of this. My <span class="strikethrough">house</span> apartment is full of projects.
+                There's not much in the way of this. My <span class="strikethrough">house</span> apartment is full of projects.<br>
+                <strong>Working with my hands</strong>
                 <ul>
                     <li>Refurbished coffee table -- Co-refurbished with my brother</li>
                     <li>A hand made computer desk &amp; dining room table -- Co-developed with the better half</li>
+                    <li>A bird feeder</li>
                     <li>A leather quiver</li>
                     <li>Various Arduino based projects like a "Soil is too dry, water me before I die" thing</li>
+                </ul>
+                <strong>Code (also working with my hands)</strong>
+                <ul>
                     <li>
-                        Numerous tinkerings on <a href="//codepen.io/wildbeard" target="_blank">my CodePen</a> -- None of these are complex but fun none the less
+                        Numerous tinkerings on <a href="//codepen.io/wildbeard" target="_blank">my CodePen</a>.<br>
+                        These range from silly images, simple games, and snippets of tools to help me when needed.
+                        <blockquote>
+                            CodePen is a definitive place for developers to showcase their work. It's like a designer being featured on Behance. It's nothing to scoff at.
+                            <span class="author">- My friend (<a href="//jonbensondesigns.com" target="_blank">jonbensondesigns.com</a>) hyping me up</span>
+                        </blockquote>
                         <ul>
                             <li>I've had a handful of Pens picked and listed right there on the homepage </li>
                             <li>I participated in the <a href="//codepen.io/collection/DKyvWY" target="_blank">Daily CSS Challenges</a> (when I had time) and most of those became Picked Pens<sup>*</sup></li>
                             <li>
                                 I also jumped onto the <a href="//codepen.io/wildbeard/pen/JNQzEZ" target="_blank">Fidget Spinner Bandwagon</a> -- What do you know it became a Picked Pen<sup>*</sup><br>
-                                I don't want to say I started a trend but after this showed up on the front page the day I made it a lot of people made their own versions. Trend. Setter. :^)
+                                Not to brag, but the day this Pen got Picked a lot of others followed and made their own. Trend. Setter. :^)
                              </li>
                             <li>Oh, and there's this <a href="//codepen.io/wildbeard/pen/BRXEGd" target="_blank">Self Portrait</a> -- A Picked Pen<sup>*</sup></li>
                             <li>Also this popular <a href="//codepen.io/wildbeard/pen/eZZPZr" target="_blank">Pen</a> -- I have no idea why it is so popular I thought it was dumb when I made it</li>
                             <li><sup>*</sup> = Picked Pens mean nothing in reality only that one of the editors saw it liked it</li>
                         </ul>
                     </li>
-                    <li>I've got a few of the typical developer side projects -- You know the ones we start but never finish.</li>
+                    <li>I've got a few of the typical developer side projects on my <a href="//github.com/wildbeard" target="_blank">Github</a> -- You know the ones we start but never finish.</li>
                 </ul>
             </li>
             <li>
                 <strong>A Blog</strong><br>
-                See #1.
+                See #1. Maybe I'll get around to writing about problems and solutions I've faced.
             </li>
             <li>
                 <strong>Testimonials</strong><br>
-                <blockquote>
-                    I still have a job
-                    <span class="author">- Myself</span>
-                </blockquote>
+                Not sure about this. I've had a consistent job since 2011. That has to mean something.
             </li>
             <li>
                 <strong>A Picture of Yourself</strong><br>
                 <blockquote>
                     Oh, and there's this <a href="//codepen.io/wildbeard/pen/BRXEGd" target="_blank">Self Portrait</a> -- A Picked Pen<sup>*</sup>
-                    <span class="author">- Myself, Again</span>
+                    <span class="author">- Me a few seconds ago</span>
                 </blockquote>
             </li>
             <li>
                 <strong>Contact Information</strong><br>
                 Email: My first &amp; last name @ gmail<br>
-                Github: <a href="//github.com/wildbeard" target="_blank">Click Here</a><br>
                 Twitter: <a href="//twitter.com/wild_beard" target="_blank">Nothing Earth shattering</a><br>
                 LinkedIn: <a href="//linkedin.com/in/prestonhaddock" target="_blank">Hey, It's Me!</a>
             </li>
