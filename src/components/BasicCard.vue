a695ebfcfb353d462d923532211ea58a7706bf1f
Preston Haddock <quotablesarcasm@gmail.com>
2024-04-05 14:30:26 -0600

    Basic card, update copy cards

---
+++ src/components/BasicCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { computed, useAttrs } from 'vue';
+
+const attrs = useAttrs();
+const classes = computed(() => {
+  const fromAttrs = attrs?.class?.split(' ') ?? [];
+  return ['p-4', 'rounded-lg', 'bg-slate-100', 'text-stone-900', ...fromAttrs];
+});
+</script>
--- src/pages/DevLogs.vue
+++ src/pages/DevLogs.vue
@@ -41,7 +41,7 @@
     <NightSky />
   </div>
 
-  <div class="bg-bossanova-600 py-8">
+  <div class="bg-bossanova py-8">
     <div class="container mx-auto px-4">
       <h2
         class="mb-8 font-hobo text-3xl text-[#f2f2f2] text-center tracking-wide leading-10">
@@ -51,71 +51,78 @@
 
     <div class="container mx-auto px-4">
       <div class="flex flex-wrap -mx-4">
-        <div class="flex w-full md:w-1/2 px-4">
-          <div class="py-2 px-4 bg-bossanova-600 rounded-lg">
-            <h3 class="mb-4 text-xl text-[#f2f2f2] leading-8">
-              Work Related
-            </h3>
-            <p class="mb-2 text-[#f2f2f2]">
-              The header above
-              <span class="italic font-semibold">says</span> Software Engineer
-              what it means is: I'm a passionate engineer who loves focusing on
-              the user experience and page performance. I enjoy the process of
-              taking designs of any kind and turning them into usuable features
-              on the web.
-            </p>
-            <p class="text-[#f2f2f2]">
-              I love building websites and tools that anyone can use and enjoy.
-              I've found myself helping out across various industries ranging
-              from sports, distilling, and creative agencies to direct to
-              consumer food delivery and outdoor landscape design.
-            </p>
+        <div class="flex flex-wrap w-full md:w-1/2 px-4">
+          <BasicCard class="mb-4">
+            <div>
+              <h3 class="mb-4 text-xl leading-8">
+                Work Related
+              </h3>
+              <p class="mb-2">
+                The header above
+                <span class="italic font-semibold">says</span> Software Engineer
+                what it means is: I'm a passionate engineer who loves focusing
+                on the user experience and page performance. I enjoy the process
+                of taking designs of any kind and turning them into usuable
+                features on the web.
+              </p>
+              <p class="">
+                I love building websites and tools that anyone can use and
+                enjoy. I've found myself helping out across various industries
+                ranging from sports, distilling, and creative agencies to direct
+                to consumer food delivery and outdoor landscape design.
+              </p>
+            </div>
+          </BasicCard>
 
-            <h3 class="my-4 text-xl text-[#f2f2f2] leading-8">
-              Work Un-related
-            </h3>
-            <p class="mb-2 text-[#f2f2f2]">
-              While not working you can usually find me spending time with my
-              better half, playing games, working on project cars, or doing
-              something outdoors. I also try and maintain a healthy amount of
-              incomplete projects (*ahem* cars) to keep myself busy when I have
-              free time.
-            </p>
+          <BasicCard>
+            <div>
+              <h3 class="my-4 text-xl leading-8">
+                Work Un-related
+              </h3>
+              <p class="mb-2">
+                While not working you can usually find me spending time with my
+                better half, playing games, working on project cars, or doing
+                something outdoors. I also try and maintain a healthy amount of
+                incomplete projects (*ahem* cars) to keep myself busy when I
+                have free time.
+              </p>
 
-            <h3 class="my-4 text-xl text-[#f2f2f2] leading-8">
-              Learnings
-            </h3>
-            <p class="mb-2 text-[#f2f2f2]">
-              <span class="block font-semibold">Game Dev</span>
-              I've always played games -- all the way back to Super Mario
-              Brothers on NES. Recently, I've been going through the process of
-              writing down ideas for a game. Are they good? Probably not. But
-              aside from that, I am learning game dev using the Godot engine.
-              Learning is slow but you checkout
-              <a
-                href="https://github.com/wildbeard/into-the-arena"
-                target="_blank"
-                class="text-[#f2f2f2] underline">the game here</a>.
-            </p>
+              <h3 class="my-4 text-xl leading-8">
+                Learnings
+              </h3>
+              <p class="mb-2">
+                <span class="block font-semibold">Game Dev</span>
+                I've always played games -- all the way back to Super Mario
+                Brothers on NES. Recently, I've been going through the process
+                of writing down ideas for a game. Are they good? Probably not.
+                But aside from that, I am learning game dev using the Godot
+                engine. Learning is slow but you checkout
+                <a
+                  href="https://github.com/wildbeard/into-the-arena"
+                  target="_blank"
+                  class="underline">the game here</a>.
+              </p>
 
-            <p class="mb-2 text-[#f2f2f2]">
-              <span class="block font-semibold">Neovim</span>
-              At the time the Vue LSP Vetur had a memory leak (or something) in
-              vscode. It would consume insane amounts of CPU and RAM on my poor
-              little Macbook. Stage left enters Neovim. I quit vscode cold
-              turkey and jumped straight into hljk. Now, anytime I need to write
-              some form of text if there's no vim-motions I feel a deep sadness.
-            </p>
-          </div>
+              <p class="mb-2">
+                <span class="block font-semibold">Neovim</span>
+                At the time the Vue LSP Vetur had a memory leak (or something)
+                in vscode. It would consume insane amounts of CPU and RAM on my
+                poor little Macbook. Stage left enters Neovim. I quit vscode
+                cold turkey and jumped straight into hljk. Now, anytime I need
+                to write some form of text if there's no vim-motions I feel a
+                deep sadness.
+              </p>
+            </div>
+          </BasicCard>
         </div>
 
-        <div class="flex w-full md:w-1/2 px-4">
-          <div class="py-2 px-4 bg-bossanova-600 rounded-lg">
-            <h3 class="mb-4 text-xl text-[#f2f2f2] leading-8">
+        <div class="flex flex-wrap w-full md:w-1/2 px-4">
+          <BasicCard class="mb-4">
+            <h3 class="mb-4 text-xl leading-8">
               Working On:
             </h3>
 
-            <p class="mb-2 text-[#f2f2f2]">
+            <p class="mb-2">
               <span class="block font-semibold">22 Tacoma 6MT:</span>
               This is my go-to truck and daily driver. While it's mostly a
               grocery getter it does serve a bigger purpose. From snow travel to
@@ -123,7 +130,7 @@
               wilderness.
             </p>
 
-            <p class="mb-2 text-[#f2f2f2]">
+            <p class="mb-2">
               <span class="block font-semibold">'93 F150:</span>
               This was my grandfather's farm truck. I've spent more time in this
               truck's bed as a kid going fishing and helping out around the farm
@@ -132,7 +139,7 @@
               summer.
             </p>
 
-            <p class="mb-2 text-[#f2f2f2]">
+            <p class="mb-2">
               <span class="block font-semibold">'95 240sx:</span>
               Arguably the worst lawn ornament I own. It doesn't run, the trunk
               doesn't latch, and there's a spare motor in the passenger seat.
@@ -142,12 +149,14 @@
               Cross. <span class="block font-semibold">The Plan:</span> There is
               no plan.
             </p>
+          </BasicCard>
 
-            <h3 class="mb-4 text-xl text-[#f2f2f2] leading-8">
+          <BasicCard>
+            <h3 class="mb-4 text-xl leading-8">
               Playing:
             </h3>
 
-            <ul class="text-[#f2f2f2]">
+            <ul class="">
               <li class="mb-2">
                 <FontAwesomeIcon
                   :icon="faSteamSquare"
@@ -155,7 +164,7 @@
                 <a
                   href="https://store.steampowered.com/app/892970/Valheim/"
                   target="_blank"
-                  class="text-[#f2f2f2] font-semibold underline">Valheim</a>
+                  class="font-semibold underline">Valheim</a>
                 It's like Minecraft but with Vikings and better in every way.
               </li>
               <li class="mb-2">
@@ -165,7 +174,7 @@
                 <a
                   href="https://store.steampowered.com/app/251570/7_Days_to_Die/"
                   target="_blank"
-                  class="text-[#f2f2f2] font-semibold underline">7DTD</a>
+                  class="font-semibold underline">7DTD</a>
                 This game is beyond scuffed. But it's fun.
               </li>
               <li class="mb-2">
@@ -175,7 +184,7 @@
                 <a
                   href="https://store.steampowered.com/app/1604030/V_Rising/"
                   target="_blank"
-                  class="text-[#f2f2f2] font-semibold underline">V-Rising</a>
+                  class="font-semibold underline">V-Rising</a>
                 Vampires &amp; Gothic Castles. Need I say more?
               </li>
               <li class="mb-2">
@@ -185,7 +194,7 @@
                 <a
                   href="https://store.steampowered.com/app/671860/BattleBit_Remastered/"
                   target="_blank"
-                  class="text-[#f2f2f2] font-semibold underline">Battlebit Remastered</a>
+                  class="font-semibold underline">Battlebit Remastered</a>
                 This is what Battlefield would look like if EA cared.
               </li>
               <li class="mb-2">
@@ -195,12 +204,12 @@
                 <a
                   href="https://store.steampowered.com/app/1063730/New_World/"
                   target="_blank"
-                  class="text-[#f2f2f2] font-semibold underline">New World</a>
+                  class="font-semibold underline">New World</a>
                 There's no better David &amp; Goliath story. Except AGS is both
                 David and Goliath.
               </li>
             </ul>
-          </div>
+          </BasicCard>
         </div>
       </div>
     </div>
@@ -210,6 +219,8 @@
 <script setup>
 // @todo: Get TS to work w/nvim
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import BasicCard from '../components/BasicCard.vue';
+
 import {
   faCodepen,
   faLinkedin,
