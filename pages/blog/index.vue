37f602e77b7e04a3a6367e5b208a649d0868ebbd
Preston Haddock <quotablesarcasm@gmail.com>
2024-07-29 16:16:03 -0600

    Layout tweaks for mobile, tablet, desktop

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -12,16 +12,16 @@
       <hr class="mt-3 mx-auto border-t-logo md:w-1/2">
     </div>
 
-    <div class="flex flex-wrap -mx-4 px-4">
-      <div class="w-full mb-4 md:w-1/4 md:order-2 md:mb-0">
+    <div class="flex flex-wrap -mx-4 px-4 lg:w-3/4 lg:mx-auto">
+      <div class="w-full md:w-1/4 md:order-2 md:mb-0">
         <h2 class="hidden mb-2 text-lg text-center md:block">
           Tags:
         </h2>
-        <div class="flex justify-center mb-4">
+        <div class="flex justify-center flex-wrap mb-4">
           <span
             v-for="tag in allTags"
             :key="tag"
-            class="mr-2 py-1 px-1.5 text-xs bg-slate-600 rounded hover:cursor-pointer"
+            class="mr-2 mb-2.5 py-1 px-1.5 text-xs bg-slate-600 rounded hover:cursor-pointer"
             :class="selectedTags.includes(tag) ? 'bg-green-400' : ''"
             @click="toggleTag(tag)">
             {{ tag }}
@@ -37,9 +37,8 @@
           v-for="post in posts"
           v-else
           :key="post.id"
-          class="md:max-w-lg mb-4">
-          <div
-            class="relative group pb-4 border-b border-b-logo hover:cursor-pointer md:border-b-transparent">
+          class="md:max-w-md mb-4 py-5 px-4 bg-bossanova-600 rounded-lg lg:bg-transparent lg:rounded-none">
+          <div class="relative group hover:cursor-pointer">
             <h3>
               <NuxtLink :to="post.path">
                 <span class="absolute inset-0"></span>
@@ -59,7 +58,9 @@
                 {{ post.dateStr }}
               </p>
 
-              <div v-if="post.tags.length" class="ml-4">
+              <div
+                v-if="post.tags.length"
+                class="w-full my-2 md:w-auto md:ml-4">
                 <span
                   v-for="tag in post.tags"
                   :key="tag"
